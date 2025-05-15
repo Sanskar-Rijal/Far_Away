6e0628e7f99caf3d9add702f55328fa2f61e5dc8
Sanskar-Rijal <rijalsanskar@gmail.com>
2025-05-15 21:52:07 +0545

    controlled elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,15 +21,42 @@ function Logo() {
 }
 
 function Form() {
+  //to use control elements we follow three steps
+  //step 1: create a state
+  //step 2: use the state to the input element that we want to control
+  //step 3: update the state when the input changes
+  const [description, setDescription] = React.useState("");
+
+  const [selected, setSelected] = React.useState(1);
+
+  //checking the new item in console
+
+  const newItem = {
+    id: Date.now(),
+    description,
+    selected,
+    packed: false,
+  };
+
+  console.log(newItem);
+
   function handleSubmit(event) {
     event.preventDefault();
+    if (!description) return;
     console.log(event);
+    setDescription("");
+    setSelected(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you Need for you Trip?</h3>
-      <select>
+      <select
+        value={selected}
+        onChange={(event) => {
+          setSelected(Number(event.target.value));
+        }}
+      >
         {/*Array(25) [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, … ] */}
         {Array.from({ length: 25 }, (currentvalue, index) => index + 1).map(
           (it) => (
@@ -37,7 +66,14 @@ function Form() {
           )
         )}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(Event) => {
+          setDescription(Event.target.value);
+        }}
+      />
       <button>ADD</button>
       {/* if i use onclick here and create
        function handleclick then it will work only when button is clicked, we want it to work when we press enter as welll */}
