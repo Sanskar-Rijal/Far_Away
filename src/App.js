8c0be378cf00ffc93a04559e5134f74e7780f517
Sanskar-Rijal <rijalsanskar@gmail.com>
2025-05-15 11:55:22 +0545

    event handler and buttons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -14,20 +19,62 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+  }
+
   return (
-    <div className="add-form">
-      <h3>What do you need for your trip 🥹</h3>
-    </div>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you Need for you Trip?</h3>
+      <select>
+        {/*Array(25) [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, … ] */}
+        {Array.from({ length: 25 }, (currentvalue, index) => index + 1).map(
+          (it) => (
+            <option value={it} key={it}>
+              {it}
+            </option>
+          )
+        )}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>ADD</button>
+      {/* if i use onclick here and create
+       function handleclick then it will work only when button is clicked, we want it to work when we press enter as welll */}
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list">list</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((it) => (
+          <Item item={it} key={it.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item(props) {
+  return (
+    <li>
+      <span
+        style={{
+          textDecoration: props.item.packed ? "line-through" : "none",
+        }}
+      >
+        {props.item.quantity} {props.item.description}
+      </span>
+      <button>{props.item.packed ? "✅" : "❌"}</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>
         YOu have addded abx items on your list and you already packed hehehe
       </em>
